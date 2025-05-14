3aa94439a981578922cc4db7969a67d5d84435ab
Алексей <142325846+melihov87@users.noreply.github.com>
2025-05-14 17:04:07 +0200

    Update get_driver.py

--- det_mir/get_driver.py
+++ det_mir/get_driver.py
@@ -1,14 +1,8 @@
 import pytest
-import functools
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 
 detmir_url = "https://www.detmir.ru/"
@@ -31,15 +25,3 @@ def driver():
     driver.get(detmir_url)
     yield driver
     driver.quit()
-
-
-def test_region_and_cookies(driver):
-    """Во всплывающем окне Регион, кликаем по кнопке "Верно" и добавляем куки"""
-    try:
-        geo_click = WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.XPATH, './/span[contains(text(), "Верно!")]')))
-        ActionChains(driver).pause(2).click(geo_click).perform()
-        driver.add_cookie(cookie)
-        driver.refresh()
-    except (TimeoutException, NoSuchElementException):
-        pytest.fail("Элемент не найден, пропускаем этот блок")
