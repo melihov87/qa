5b67e444b146ac5055740359e4d1d962c0b4c699
Алексей <142325846+melihov87@users.noreply.github.com>
2025-05-22 17:04:04 +0200

    Add files via upload

---
+++ aviasales/locator.py
@@ -0,0 +1,13 @@
+URL = "https://www.aviasales.ru/"
+WHERE_FROM = "[data-test-id='origin-input']"
+WHERE = "[data-test-id='destination-input']"
+INPUT_WHERE_FROM = "[data-test-id='suggested-city-TGD']"
+INPUT_WHERE = '#avia_form_destination-input'
+DATE = '[data-test-id="start-date-field"]'
+DATE_START = '[data-test-id="date-23.05.2025"]'
+DATE_END = '[data-test-id="date-24.05.2025"]'
+ADULTS = "[data-test-id='number-of-adults'] [data-test-id='increase-button']"
+CHILDREN = "[data-test-id='number-of-children'] [data-test-id='increase-button']"
+INFANTS = "[data-test-id='number-of-infants'] [data-test-id='increase-button']"
+PASSENGERS = "[data-test-id='passengers-field']"
+BUTTON_SEARCH = "[data-test-id='form-submit']"
